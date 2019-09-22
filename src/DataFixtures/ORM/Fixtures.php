9673ec1eb2cc478c9ed365f98dae4588c2b8f8ed
astabile <alejandrostabile@gmail.com>
2019-09-22 19:38:17 -0300

    Initial stage: - Database configuration - Fixture with dummy data - User and Entry entities - Blog controller - Repository files - Initial template

---
+++ src/DataFixtures/ORM/Fixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures\ORM;
+
+use App\Entity\User;
+use App\Entity\Entry;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class Fixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user
+            ->setUsername('admin')
+            ->setEmail('alejandrostabile@gmail.com')
+            ->setPassword('pass')
+            ->setTwitter('john.doe');
+
+        $manager->persist($user);
+
+        $entry = new Entry();
+        $entry
+            ->setTitle('First blog post example')
+            ->setContent('Lorem Ipsum is simply dummy text of the printing and typesetting industry.')
+            ->setUser($user);
+        $manager->persist($entry);
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Entry.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Entry
+ *
+ * @ORM\Table(name="Entry")
+ * @ORM\Entity(repositoryClass="App\Repository\EntryRepository")
+ * @ORM\HasLifecycleCallbacks
+ */
+class Entry
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="creation_date", type="datetimetz")
+     */
+    private $creationDate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="string", length=2000)
+     */
+    private $content;
+
+    /**
+     * @var User
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    private $user;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set creationDate
+     *
+     * @param \DateTime $creationDate
+     *
+     * @return Entry
+     */
+    public function setCreationDate($creationDate)
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get creationDate
+     *
+     * @return \DateTime
+     */
+    public function getCreationDate()
+    {
+        return $this->creationDate;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function prePersist()
+    {
+        if (!$this->getCreationDate()) {
+            $this->setCreationDate(new \DateTime());
+        }
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Entry
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Entry
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set user
+     *
+     * @param User $user
+     *
+     * @return Entry
+     */
+    public function setUser(User $user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
